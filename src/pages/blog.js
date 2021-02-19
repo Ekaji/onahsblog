1ebd669ec364dbc2cdf357025a66c7f8921b0c63
Ekaji <ekajionah@gmail.com>
2021-02-19 13:25:41 +0100

    added tags to blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,19 +3,27 @@ import { graphql, Link } from "gatsby"
 import Footer from '../components/layout/Footer'
 import styles from '../styles/grid.module.css'
 import "normalize.css"
+import '../styles/blog.styles.css'
+
+// import kebabCase from "lodash/kebabCase"
 
 export default function Blog({ data }) {
   const { posts } = data.blog;
+  let allTags = posts.map(post => post.frontmatter.tags )
+  let uniqueTags = [...new Set(allTags)]
+  console.log(uniqueTags)
 
   return (
-    <div>
+    <div className='blog'>
       <h1>Onahs Blog</h1>
       <h2>Articles</h2>
+     
      <div className={styles.articleGrid}>
       {posts.map(post => (
         <Link  to={post.fields.slug} >
           <article key={post.id}>
             <h2>{post.frontmatter.title}</h2>
+            <h4>{post.frontmatter.tags}</h4>
             <small>{post.frontmatter.author}, {post.frontmatter.date}</small>
             <p>{post.excerpt}</p>
           </article>
@@ -24,6 +32,21 @@ export default function Blog({ data }) {
         )
       }
      </div>
+     <aside>
+        <h3>Tags</h3>
+        <ul>
+          {posts.map((post, i) => (
+            
+            (<li key={post.id} style={{listStyleType: 'none'}}>
+              <Link to={`/tags/${post.frontmatter.tags}`}>
+                {uniqueTags[i]} 
+              </Link>
+            </li>)
+          )
+         )
+        }
+        </ul>
+      </aside>
      <Footer />
     </div>
   )
